a5a6817689e046e7ffae014aba20fbf77762d25b
strangerthandev115 <139648342+strangerthandev115@users.noreply.github.com>
2024-11-13 14:51:27 -0600

    fixed weird init setup

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 import os
 SECRET_KEY = os.urandom(24)
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Fusionman361@localhost/leaderboard' #Change password to your password for MySQL workbench
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from app import app
-import config
 
 if __name__ == '__main__':
-    app.config.from_object(config)
     app.run(debug=True)
